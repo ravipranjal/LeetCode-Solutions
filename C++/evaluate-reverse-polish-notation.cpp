d9966a21c2fdfde731acac60c5c4e91077e0fa67
ravipranjal <ravipranjalp@gmail.com>
2021-11-20 16:57:13 -0600

    fix bug

--- C++/evaluate-reverse-polish-notation.cpp
+++ C++/evaluate-reverse-polish-notation.cpp
@@ -1,21 +1,3 @@
-// Source : https://oj.leetcode.com/problems/evaluate-reverse-polish-notation/
-// Author : Hao Chen
-// Date   : 2014-06-16
-
-/********************************************************************************** 
-* 
-* Evaluate the value of an arithmetic expression in Reverse Polish Notation.
-* 
-* Valid operators are +, -, *, /. Each operand may be an integer or another expression.
-* 
-* Some examples:
-* 
-*   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
-*   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
-* 
-*               
-**********************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
