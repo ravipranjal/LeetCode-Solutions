871b1d9b8fdade0e611c1469ef041486fd9d52d9
ravipranjal <ravipranjalp@gmail.com>
2021-11-20 19:38:49 -0600

    fix bug

--- C++/coin-change-2.cpp
+++ C++/coin-change-2.cpp
@@ -1,16 +1,34 @@
 // Time:  O(n * m)
 // Space: O(m)
 
-class Solution {
-public:
-    int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount + 1);
-        dp[0] = 1;
-        for (const auto& coin : coins) {
-            for (int i = coin; i <= amount; ++i) {
-                dp[i] += dp[i - coin];
-            }
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+int change(int amount, vector<int>& coins, int n) {
+    vector<int> dp(amount + 1);
+    dp[0] = 1;
+    for (int j=0; j<n; j++) {
+        int coin = coins[j];
+        for (int i = coin; i <= amount; ++i) {
+            dp[i] += dp[i - coin];
         }
-        return dp[amount];
     }
-};
+    return dp[amount];
+}
+
+int main() {
+    vector<int> coins(5);
+    int n, amount = 100;
+    cout<<"Enter the number of coins: ";
+    cin>>n;
+
+    for(int i=0; i<n; i++) {
+        scanf("%d", &coins[i]);
+    }
+    cout<<"Minimum coin change required is: "<<change(amount, coins, n);
+
+    return 0;
+}
