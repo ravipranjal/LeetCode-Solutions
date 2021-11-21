f01c71503fbdcee0567831c0147d98aa4a050666
ravipranjal <ravipranjalp@gmail.com>
2021-11-20 20:00:23 -0600

    fix bug

--- C++/compare-strings-by-frequency-of-the-smallest-character.cpp
+++ C++/compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,25 +1,72 @@
 // Time:  O((m + n)logn), m is the number of queries, n is the number of words
 // Space: O(n)
 
-class Solution {
-public:
-    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
-        vector<int> words_freq;
-        for (const auto& word : words) {
-            words_freq.emplace_back(f(word));
-        }
-        sort(words_freq.begin(), words_freq.end());
-        vector<int> result;
-        for (const auto& query : queries) {
-            const auto& it = upper_bound(words_freq.cbegin(), words_freq.cend(), f(query));
-            result.emplace_back(distance(it, words_freq.cend()));
-        }
-        return result;
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+/*Helper function to calculate frequency of smallest element*/
+int getF(string &s){
+    int a[26] = {0,};
+    for(int i = 0; i < s.size(); i++){
+        a[s[i]-'a']++;
+    }
+    
+    for(int i = 0; i < 26; i++){
+        if(a[i] != 0) return a[i];
+    }
+    return 0;
+}
+
+vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        vector<int> fr(12, 0);
+    int f;
+    vector<int> q;
+    for(int i = 0; i < words.size(); i++){
+        f = getF(words[i]);
+        fr[f]++;
+    }
+    //Find cumulative frequency i.e. ith item will have sum of i..end.
+    for(int i = 9; i >= 0; i--){
+        fr[i] = fr[i] + fr[i+1];
+    }
+    
+    for(int i = 0; i < queries.size(); i++){
+        f = getF(queries[i]);
+        q.push_back(fr[f+1]);
+    }
+    return q;
+}
+
+
+int main() {
+    char queries[5][5];
+    char words[5][5];
+    int n;
+    cout<<"Enter the number of queries: ";
+    cin>>n;
+
+    vector<string> q;
+    vector<string> w;
+
+    cout<<"Enter query and word: "<<endl;
+    for(int i=0; i<n; i++) {
+        scanf("%50s", queries[i]);
+        q.push_back(queries[i]);
+
+        scanf("%50s", words[i]);
+        w.push_back(words[i]);
     }
 
-private:
-    int f(const string& word) {
-        const auto& smallest = *min_element(word.cbegin(), word.cend());
-        return count(word.cbegin(), word.cend(), smallest);
+
+    vector<int> ans = numSmallerByFrequency(q, w);
+
+    cout<<"Answer is: "<<endl;
+    for(int i=0; i<ans.size(); i++) {
+        cout<<ans[i]<<" ";
     }
-};
+
+    return 0;
+}
